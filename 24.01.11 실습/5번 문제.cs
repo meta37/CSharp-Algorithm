312942e8b4ffe57b28b42fe01d9483fd69694290
minsang kim <mskim3747@gmail.com>
2024-01-11 17:39:10 +0900

    feat : 4번 문제 완료

---
+++ 24.01.11 실습/5번 문제.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._01._11_실습
+{
+    internal class _5번_문제
+    {
+        // 사용자의 입력을 받아서 없는 데이터면 추가, 있던 데이터면 삭제하는 리스트
+        // a. ex) 1, 6, 7, 8, 3 들고 있던 상황이면
+        // b. 2입력하면 없던 경우니까 1, 6, 7, 8, 3, 2
+        // c. 7입력하면 있던 경우니까 1, 6, 8, 3
+        static void Main1()
+        {
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+            list.Add(3);
+
+            Console.WriteLine("숫자를 입력하세요 : ");
+            int input = int.Parse(Console.ReadLine());
+            int num = int.Parse(Console.ReadLine());
+
+            if (!list.Contains(num))
+            {
+                list.Add(num);
+            }
+            else list.Remove(num);
+            foreach (int i in list) { Console.WriteLine(i); }
+        }
+    }
+}
--- 24.01.11 실습/Program.cs
+++ 24.01.11 실습/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _24._01._11_실습
+﻿using System.ComponentModel;
+
+namespace _24._01._11_실습
 {
     internal class Program
     {
@@ -6,10 +8,24 @@
         // 0부터 숫자까지 가지는 리스트를 만들기
         // 0요소를 제거
         // 리스트가 가지는 모든 요소들을 출력해주는 반복문 작성 add remove 접근
-
+        
         static void Main(string[] args)
         {
-            
+            List<int> list = new List<int>();
+
+            Console.Write("숫자를 입력하세요 : ");
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= input; i++)
+            {
+                list.Add(i);
+            }
+            list.Remove(0);
+            for (int i = 0;i < list.Count; i++)
+            {
+                Console.Write(list[i]);
+            }
+
         }
     }
 }
