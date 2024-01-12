898c1c43ac928a63fc107ccbc8b43c70c894eb86
minsang kim <mskim3747@gmail.com>
2024-01-12 17:53:09 +0900

    feat : 실습

--- 24.01.12 실습/요세푸스 순열 문제.cs
+++ 24.01.12 실습/요세푸스 순열 문제.cs
@@ -10,6 +10,8 @@ namespace _24._01._12_실습
     {
         public void main1()
         {
+            int n = 7;
+            int k = 3;
             LinkedList<int> linkedList = new LinkedList<int>();
             Console.WriteLine("사람의 수를 입력하세요 : ");
             int num = int.Parse(Console.ReadLine());
@@ -19,6 +21,26 @@ namespace _24._01._12_실습
             for (int i = 0; i < num; i++)
             {
                 linkedList.AddLast(i);
+            }
+
+            while (linkedList.Count > 0)
+            {
+                for (int i = 0; i <= k; i++)
+                {
+                    LinkedListNode<int> node = linkedList.First;
+                    if(i == k)
+                    {
+                        // 빠지기
+                        linkedList.Remove(node);
+                        Console.Write($"{node.Value} ");
+                    }
+                    else
+                    {
+                        // 뒤로 들어가기
+                        linkedList.Remove(node);
+                        linkedList.AddLast(node);
+                    }
+                }
             }
 
         }
