fd8a6a1b1181b6dd529148bf0ce0c66c5e93ac7b
minsang kim <mskim3747@gmail.com>
2024-01-15 17:59:53 +0900

    feat : 01.15 실습 완료

---
+++ 24.01.15 실습/3번 문제.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._01._15_실습
+{
+    internal class _3번_문제
+    {
+        public const int workTime = 8;
+
+        public static int[] ProcessJob(int[] jobList)
+        {
+            Queue<int> queue = new Queue<int>();
+            int remainTime = 0;
+            int day = 1;
+            List<int> days = new List<int>();
+
+            for (int i = 0; i < jobList.Length; i++)
+            {
+                queue.Enqueue(jobList[i]);
+            }
+
+            while (queue.Count > 0)
+            {
+                int workTime=queue.Dequeue();
+                while (true)
+                {
+                    if (workTime <= remainTime)
+                    {
+                        remainTime -= workTime;
+                        // 작업 완료
+                        days.Add(day);
+                        break;
+                    }
+                    else
+                    {
+                        workTime -= remainTime;
+                        // 다음 날로
+                        day++;
+                        remainTime = 8;
+                    }
+                }
+            }
+            return days.ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            int[] result = ProcessJob(new int[] { 4, 4, 12, 10, 2, 10 });
+            foreach (int day in result)
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
--- 24.01.15 실습/Program.cs
+++ 24.01.15 실습/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _24._01._15_실습
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _24._01._15_실습
 {
     internal class Program
     {
@@ -8,9 +10,46 @@
         // c. 추가(3,2,1), 꺼내기(1번), 추가(6,5,4), 꺼내기(3번), 추가(9,8,7), 모두꺼내기 : 스택 : 1, 4, 5, 6, 7, 8, 9, 2, 3 큐 : 3, 2, 1, 6, 5, 4, 9, 8, 7
         // d. 추가(1,3,5), 꺼내기(1번), 추가(2,4,8), 꺼내기(2번), 추가(1,3), 모두꺼내기 : 스택 : 5, 8, 4, 3, 1, 2, 3, 1 큐 : 1, 3, 5, 2, 4, 8, 1, 3
         // e. 추가(3,2,1), 꺼내기(2번), 추가(3,2,1), 꺼내기(2번), 추가(3,2,1), 모두꺼내기 : 스택 : 1, 2, 1, 2, 1, 3, 3 큐 : 3, 2, 1, 3, 2, 1
+        
+        public static bool IsOk(string text)
+        {
+            Stack<char> stack = new Stack<char>();
+            char temp;
+            foreach (char c in text)
+            {
+                if (c == '(' || c == '{' || c == '[')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    temp = stack.Peek();
+                    if (temp == '(' && c == ')') { stack.Pop(); }
+                    else if (temp == '{' && c == '}') { stack.Pop(); }
+                    else if (temp == '[' && c == ']') { stack.Pop(); }
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            else return false;
+        }
 
         static void Main(string[] args)
         {
+            Console.Write("괄호를 입력하세요 : ");
+            string input = Console.ReadLine();
+            if (IsOk(input))
+            {
+                Console.WriteLine("완성입니다.");
+            }
+            else
+            {
+                Console.WriteLine("미완성입니다.");
+            }
         }
+
     }
 }
