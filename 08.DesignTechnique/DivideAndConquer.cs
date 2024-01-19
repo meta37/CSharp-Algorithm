1db176a0725cbba9f21c24bcd85ff69ffd9b5f12
minsang kim <mskim3747@gmail.com>
2024-01-19 14:22:10 +0900

    feat : DesignTechnique

---
+++ 08.DesignTechnique/DivideAndConquer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.DesignTechnique
+{
+    internal class DivideAndConquer
+    {
+        /*************************************************************************
+		 * 분할정복 (Divide and Conquer)
+		 * 
+		 * 큰 문제를 작은 문제로 나눠서 푸는 하향식 접근 방식
+		 * 분할을 통해서 해결하기 쉬운 작은 문제로 나눈 후 정복한 후 병합하는 과정
+		 **************************************************************************/
+
+        // 예시 - 거듭 제곱
+        int Pow(int x, int n)       // 0(logn)
+        {
+            if (n == 1)
+            {
+                return x;
+            }
+
+            int result = Pow(x, n / 2);
+            return Pow(x, n/2 ) * Pow (x, n/2 );
+
+            if (n % 2 == 0)
+            {
+                return Pow(x * x, n / 2);
+            }
+            else
+            {
+                return Pow(x * x, n / 2) * x;
+            }
+        }
+
+        int Power(int x, int n)     // 시간복잡도 0(n)
+        {
+            int result = 1;
+            for (int i = 0; i < n; i++)
+            {
+                result *= x;
+            }
+            return result;
+        }
+    }
+}
--- 코딩테스트/Program.cs
+++ 코딩테스트/Program.cs
@@ -1,7 +1,40 @@
-﻿namespace 코딩테스트
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace 코딩테스트
 {
     internal class Program
     {
-        public class 
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] values = { 1, 2, 3, 4, 5, -99, 8 };
+            int max = int.MinValue;
+
+            // ex) [2, 4] : 2 ~ 4 더한 값
+            int[,] result = new int[values.Length, values.Length];
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                result[i , i ] = values[i];
+                if (max < values[i])
+                {
+                    max = values[i];
+                }
+            }
+
+            for (int start = 0; start < values.Length; start++)
+            {
+                for (int end = start + 1; end < values.Length; end++)
+                {
+                    result[start , end ] = result[start, end -1 ] + values[end];
+                    if (max < result[start, end])
+                    {
+                        max = result[start, end];
+                    }
+                }
+            }
+
+            Console.WriteLine(max);
+        }
     }
 }
